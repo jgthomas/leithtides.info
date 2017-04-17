f7b6c5f0d99fe84864915c8b5e305a01b00930f9
James Thomas <dairy_water@yahoo.co.uk>
2017-04-17 10:20:22 +0100

    get times now function, so calls when needed

--- tide_times_leith.py
+++ tide_times_leith.py
@@ -15,27 +15,30 @@ LT_MATCH = re.compile(r'([0-9][0-9]:[0-9][0-9]) - Low Tide')
 
 URL = 'https://www.tidetimes.org.uk/leith-tide-times.rss'
 
-tide = feedparser.parse(URL)
 
-tide_data = tide['entries'][0]['summary']
+def get_tide_times(url):
+    tide = feedparser(url)
+    tide_data = tide['entries'][0]['summary']
+    high_tide = re.findall(HT_MATCH, tide_data)
+    low_tide = re.findall(LT_MATCH, tide_data)
+    return (low_tide, high_tide)
 
-high_tide = re.findall(HT_MATCH, tide_data)
-low_tide = re.findall(LT_MATCH, tide_data)
 
-
-def tide_message(times):
-    if len(times) == 2:
-        first, second = times
+def tide_message(tides):
+    low, high = tides
+    if len(low) == 2:
+        first, second = low
         msg = 'low tide in leith is at {} and at {}'.format(first, second)
     else:
-        first, *_ = times
+        first, *_ = low
         msg = 'low tide in leith is at {}'.format(first)
     return msg
 
 
 @ask.launch
 def tide_report():
-    welcome_msg = tide_message(low_tide)
+    tides = get_tide_times(URL)
+    welcome_msg = tide_message(tides)
     return statement(welcome_msg)
 
 
