10682ac768373191965253f09e1cdc6b49b6e692
James Thomas <dairy_water@yahoo.co.uk>
2017-05-20 07:06:02 +0100

    added new tests

--- tests/test_weight_tides.py
+++ tests/test_weight_tides.py
@@ -7,19 +7,44 @@ from tide_response import weight_tides
 
 
 class Test(unittest.TestCase):
-    def test_first_not_day_second_closer(self):
+    def test_first_long_past(self):
         tide_times = [dt.datetime(1900, 1, 1, 2, 31), dt.datetime(1900, 1, 1, 15, 5)]
         now = dt.datetime(1900, 1, 1, 13, 58)
         self.assertEqual((0, 1), weight_tides(tide_times, now))
 
-    def test_both_day_first_hour_ago(self):
+    def test_first_hour_ago(self):
         tide_times = [dt.datetime(1900, 1, 1, 12, 58), dt.datetime(1900, 1, 1, 21, 55)]
         now = dt.datetime(1900, 1, 1, 13, 58)
         self.assertEqual((1, 0), weight_tides(tide_times, now))
 
-    def test_first_day_closer_over_two_hours_ago(self):
+    def test_first_over_two_hours_ago(self):
         tide_times = [dt.datetime(1900, 1, 1, 9, 58), dt.datetime(1900, 1, 1, 22, 55)]
-        now = dt.datetime(1900, 1, 1, 14, 00)
+        now = dt.datetime(1900, 1, 1, 13, 58)
+        self.assertEqual((0, 1), weight_tides(tide_times, now))
+        
+    def test_first_in_future_long(self):
+        tide_times = [dt.datetime(1900, 1, 1, 19, 58), dt.datetime(1900, 1, 1, 22, 55)]
+        now = dt.datetime(1900, 1, 1, 13, 58)
+        self.assertEqual((1, 0), weight_tides(tide_times, now))
+
+    def test_first_in_future_very_close(self):
+        tide_times = [dt.datetime(1900, 1, 1, 13, 59), dt.datetime(1900, 1, 1, 22, 55)]
+        now = dt.datetime(1900, 1, 1, 13, 58)
+        self.assertEqual((1, 0), weight_tides(tide_times, now))
+
+    def test_first_now(self):
+        tide_times = [dt.datetime(1900, 1, 1, 13, 58), dt.datetime(1900, 1, 1, 22, 55)]
+        now = dt.datetime(1900, 1, 1, 13, 58)
+        self.assertEqual((1, 0), weight_tides(tide_times, now))
+
+    def test_second_in_past(self):
+        tide_times = [dt.datetime(1900, 1, 1, 1, 58), dt.datetime(1900, 1, 1, 12, 55)]
+        now = dt.datetime(1900, 1, 1, 13, 58)
+        self.assertEqual((0, 1), weight_tides(tide_times, now))
+
+    def test_second_long_past(self):
+        tide_times = [dt.datetime(1900, 1, 1, 1, 58), dt.datetime(1900, 1, 1, 2, 55)]
+        now = dt.datetime(1900, 1, 1, 13, 58)
         self.assertEqual((0, 1), weight_tides(tide_times, now))
 
 
